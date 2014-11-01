75b0d48b52bdd8221f4ca69421f63129a5db8c02
Gabriel Rodrigues <gabrielsr@gmail.com>
2014-11-01 13:36:26 -0200

    Incluindo alguns assets

---
+++ desktop/src/br/unb/bomberman/ui/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package br.unb.bomberman.ui.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import br.unb.bomberman.ui.screens.GDXGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GDXGame(), config);
+	}
+}
