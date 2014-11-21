c69463b17a356a2413e9af8910ad36f3177b7782
Denis Albuquerque <denisalbuquerque@gmail.com>
2014-11-21 01:29:52 -0200

    Adding the Menu of Main Game Screen
    This class needs refactory

--- core/src/br/unb/bomberman/ui/screens/MainMenuScreen.java
+++ core/src/br/unb/bomberman/ui/screens/MainMenuScreen.java
@@ -4,14 +4,26 @@ import br.unb.unbomber.GDXGame;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class MainMenuScreen implements Screen {
 
+	final GDXGame game;
+	
 	private static final int    FRAME_COLS = 4;     // #1
     private static final int    FRAME_ROWS = 1;     // #2
     
@@ -21,9 +33,6 @@ public class MainMenuScreen implements Screen {
     private static final int   DOWN_Y = 30;
     
     Texture           background;
-	
-    final GDXGame game;
-    
     OrthographicCamera camera;
     
     Animation           walkAnimationRightMoonWalker;
@@ -38,15 +47,26 @@ public class MainMenuScreen implements Screen {
 	
 	TextureRegion       currentFrameMoonWalker; 
 	TextureRegion       currentFrameZombie; 
-	
+
 	int initialPositionMoonWalker_X = DOWN_X;
 	int initialPositionMoonWalker_Y = DOWN_Y;
 	int initialPositionZombie_X = TOP_X;
 	int initialPositionZombie_Y = TOP_Y;
-	
 	boolean isMoonWalkerWalkingRight = true;
 	int steps = 0;
 	float stateTime = 0f;
+	
+	// Main Menu Components
+	final int INIT_POS_X_MENU = 160;
+	final int INIT_POS_Y_MENU = 290;
+	final int MENU_ITEN_HEIGHT = 50;
+	final int MENU_ITEN_WIDTH = 350;
+	private Stage stage;
+	private Skin skin;
+	private Pixmap pixmap;
+	private TextButton button;
+	private TextButtonStyle textButtonStyle;
+	
 
     public MainMenuScreen(final GDXGame game) {
         this.game = game;
@@ -55,22 +75,86 @@ public class MainMenuScreen implements Screen {
         camera.setToOrtho(false, 800, 480);
         
         //The background
-        background = new Texture(Gdx.files.local("background.png"));
-        
+        background = new Texture(Gdx.files.local("background.png"));       
         // The MoonWalker
         walkSheetRightMoonWalker = new Texture(Gdx.files.local("data/character1/walking-left.png"));
         walkAnimationRightMoonWalker = createWalkingFrame( walkSheetRightMoonWalker);
-        
         walkSheetLeftMoonWalker = new Texture(Gdx.files.local("data/character1/walking-right.png"));
         walkAnimationLeftMoonWalker = createWalkingFrame( walkSheetLeftMoonWalker);
-
         // The Zoombie
         walkSheetRightZombie = new Texture(Gdx.files.internal("data/character2/walking-right.png"));
         walkAnimationRightZombie = createWalkingFrame( walkSheetRightZombie);
-
         walkSheetLeftZombie = new Texture(Gdx.files.internal("data/character2/walking-left.png"));
         walkAnimationLeftZombie = createWalkingFrame( walkSheetLeftZombie);
         
+        // Game Main Menu
+        //TODO: refactoring 
+        stage = new Stage();
+        stage.clear();
+        Gdx.input.setInputProcessor(stage);
+        pixmap = new Pixmap(100, 100, Format.RGBA8888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.fill();
+		skin = new Skin();
+		skin.add("white", new Texture(pixmap));
+        textButtonStyle = new TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+		textButtonStyle.checked = skin.newDrawable("white", Color.BLACK);
+		textButtonStyle.font = new BitmapFont(Gdx.files.internal("data/font.fnt"), Gdx.files.internal("data/font.png"), false);
+		textButtonStyle.font.setScale(1.6f);
+        button = new TextButton("New Game", textButtonStyle);
+        button.setPosition(INIT_POS_X_MENU, INIT_POS_Y_MENU-MENU_ITEN_HEIGHT*0);
+        button.setHeight(MENU_ITEN_HEIGHT);
+        button.setWidth(MENU_ITEN_WIDTH);
+        button.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Assets.playSound(Assets.clickSound);
+				Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
+				Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+				game.getScreen().dispose();
+				game.setScreen(new GameScreen(game, game.FIRST_STAGE_LEVEL_ID));
+			}
+		});
+        stage.addActor(button);
+        button = new TextButton("High Scores", textButtonStyle);
+        button.setPosition(INIT_POS_X_MENU, INIT_POS_Y_MENU-MENU_ITEN_HEIGHT*1);
+        button.setHeight(MENU_ITEN_HEIGHT);
+        button.setWidth(MENU_ITEN_WIDTH);
+        button.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Assets.playSound(Assets.clickSound);
+				game.getScreen().dispose();
+			}
+		});
+        stage.addActor(button);
+        button = new TextButton("Settings", textButtonStyle);
+        button.setPosition(INIT_POS_X_MENU, INIT_POS_Y_MENU-MENU_ITEN_HEIGHT*2);
+        button.setHeight(MENU_ITEN_HEIGHT);
+        button.setWidth(MENU_ITEN_WIDTH);
+        button.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Assets.playSound(Assets.clickSound);
+				game.getScreen().dispose();
+			}
+		});
+        stage.addActor(button);
+        button = new TextButton("How to Play", textButtonStyle);
+        button.setPosition(INIT_POS_X_MENU, INIT_POS_Y_MENU-MENU_ITEN_HEIGHT*3);
+        button.setHeight(MENU_ITEN_HEIGHT);
+        button.setWidth(MENU_ITEN_WIDTH);
+        button.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Assets.playSound(Assets.clickSound);
+				game.getScreen().dispose();
+			}
+		});
+        stage.addActor(button);
     }
     
     private Animation createWalkingFrame( Texture walkSheet){
@@ -100,27 +184,20 @@ public class MainMenuScreen implements Screen {
         
         //inverte quando chegar no final
         if(isMoonWalkerWalkingRight && initialPositionMoonWalker_X + steps >= TOP_X){
-        	
         	initialPositionMoonWalker_X = TOP_X;
         	initialPositionMoonWalker_Y = TOP_Y;
         	initialPositionZombie_X = DOWN_X;
         	initialPositionZombie_Y = DOWN_Y;
-        	
         	isMoonWalkerWalkingRight = false;
-        	steps = 0;
-        	
+        	steps = 0;        	
         }else if( !isMoonWalkerWalkingRight && initialPositionMoonWalker_X - steps <= DOWN_X){
-        	
         	initialPositionMoonWalker_X = DOWN_X;
         	initialPositionMoonWalker_Y = DOWN_Y;
         	initialPositionZombie_X = TOP_X;
         	initialPositionZombie_Y = TOP_Y;
-        	
         	isMoonWalkerWalkingRight = true;
         	steps = 0;
-        	
         }
-        
         if(isMoonWalkerWalkingRight){
         	currentFrameMoonWalker = walkAnimationRightMoonWalker.getKeyFrame(stateTime, true);
         	currentFrameZombie = walkAnimationLeftZombie.getKeyFrame(stateTime, true);
@@ -130,9 +207,7 @@ public class MainMenuScreen implements Screen {
         }
         
         game.batch.begin();
-        
         game.batch.draw(background, -28, -122, 864, 720);
-        
         if(isMoonWalkerWalkingRight){
         	game.batch.draw(currentFrameMoonWalker, initialPositionMoonWalker_X + steps, initialPositionMoonWalker_Y, 154, 126); 
         	game.batch.draw(currentFrameZombie, initialPositionZombie_X - steps, initialPositionZombie_Y, 154, 126); 
@@ -140,20 +215,10 @@ public class MainMenuScreen implements Screen {
         	game.batch.draw(currentFrameMoonWalker, initialPositionMoonWalker_X - steps, initialPositionMoonWalker_Y, 154, 126); 
         	game.batch.draw(currentFrameZombie, initialPositionZombie_X + steps, initialPositionZombie_Y, 154, 126);
         }
-        
-        
-        
-        // Menu... TODO: Criar botões reais.
-        Assets.font.draw(game.batch, "New Game!", 50, 300);
-        Assets.font.draw(game.batch, "High Scores", 50, 250);
-        Assets.font.draw(game.batch, "How to Play", 50, 200);
-        
+        Assets.font.draw(game.batch, "NEED FIX: Why the zombie disapear if we delete/comment this line?", 0, 0);
+        stage.act();
+        stage.draw();
         game.batch.end();
-
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new GameScreen(game, game.FIRST_STAGE_LEVEL_ID));
-            dispose();
-        }
     }
 
 	@Override
@@ -188,11 +253,9 @@ public class MainMenuScreen implements Screen {
 
 	@Override
 	public void dispose() {
+		stage.dispose();
 		// TODO Auto-generated method stub
 		
 	}
-
-
         //...Rest of class omitted for succinctness.
-
 }
