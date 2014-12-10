472411c6aea66c1871727bd166b935fcdd3c94ca
Gabriel Rodrigues <gabrielsr@gmail.com>
2014-12-10 02:36:47 -0200

    Changing the load map to be a system

--- core/src/br/unb/unbomber/systems/LoadStageSystem.java
+++ core/src/br/unb/unbomber/systems/LoadStageSystem.java
@@ -1,5 +1,9 @@
 package br.unb.unbomber.systems;
 
+import br.unb.unbomber.component.CellPlacement;
+import br.unb.unbomber.component.Draw;
+import br.unb.unbomber.component.ExplosionBarrier;
+import br.unb.unbomber.component.ExplosionBarrier.ExplosionBarrierType;
 import br.unb.unbomber.core.BaseSystem;
 import br.unb.unbomber.core.Entity;
 import br.unb.unbomber.core.EntityManager;
@@ -22,6 +26,7 @@ public class LoadStageSystem extends BaseSystem {
 
 	StageSpec stage;
 
+	
 	public LoadStageSystem(EntityManager em, String stageId) {
 		super(em);
 
@@ -29,7 +34,47 @@ public class LoadStageSystem extends BaseSystem {
 		Json json = new Json();
 		FileHandle stageFile = Gdx.files.local(stageId + ".json");
 		this.stage = json.fromJson(StageSpec.class, stageFile.reader());
+		addBlocks(em, this.stage.getMapRepresentation());
+	}
+	
+	public void addBlocks(EntityManager entityManager, String map){
 
+		int x = 0;
+		int y = 0;
+		
+		
+		for (int i = 0; i < map.length(); i++) {
+			if(map.charAt(i) == '#') {
+				addBlock(x,y,ExplosionBarrierType.BLOCKER, "hardBlock");
+			}
+			else if (map.charAt(i) == '@') {
+				addBlock(x,y,ExplosionBarrierType.STOPPER, "softBlock");
+			}
+			
+			if (map.charAt(i) == '\n') {
+				y += 1;
+				x = 0;
+			}
+			else {
+				x += 1;
+			}
+		}
+	}
+	
+	public void addBlock(int x, int y, ExplosionBarrierType explosionBarrierType, String drawType){
+		Entity block = getEntityManager().createEntity();
+		
+		CellPlacement cell = new CellPlacement();
+		
+		cell.setCellX(x);
+		cell.setCellY(y);
+		
+		ExplosionBarrier explosionBarrier = new ExplosionBarrier();
+		explosionBarrier.setType(explosionBarrierType);
+	
+		block.addComponent(new Draw("hardBlock"));
+		block.addComponent(cell);
+		getEntityManager().update(block);
 	}
 
 	@SuppressWarnings("deprecation")
