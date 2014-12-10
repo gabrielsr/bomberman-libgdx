1e569838666be8704262a25e70076e60fa7698a5
jcosta9 <jpaulo.caraujo@gmail.com>
2014-12-10 21:03:13 -0200

    Paginas WinAMatch e WinACup
    Bem simplificadas. Falta testar e colocar uma animação

--- core/src/br/unb/bomberman/ui/screens/WinScreen.java
+++ core/src/br/unb/bomberman/ui/screens/WinScreen.java
@@ -2,54 +2,97 @@ package br.unb.bomberman.ui.screens;
 
 import br.unb.unbomber.GDXGame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class WinScreen implements Screen {
 
-	public WinScreen(GDXGame game) {
-		// TODO Auto-generated constructor stub
-	}
 
-	@Override
-	public void render(float delta) {
-		// TODO Auto-generated method stub
+		final GDXGame game;
+		
+		Texture           background;
+	    OrthographicCamera camera;
+	    
+	    private Stage stage;
+	    
+	    public WinScreen (final GDXGame game){
+	    	this.game = game;
 
-	}
+	        camera = new OrthographicCamera();
+	        camera.setToOrtho(false, 800, 480);
+	        
+	        //The background
+	        background = new Texture(Gdx.files.local("win.png"));
+	        
+	        // Game Main Menu Building
+	        stage = new Stage();
+	        stage.clear();
+	        Gdx.input.setInputProcessor(stage);
+	        MenuButtonFactory factory = new MenuButtonFactory();
+	        stage.addActor(factory.makeMenuButton(game, "Play Again", new GameScreen(game, game.FIRST_STAGE_LEVEL_ID)));
+	        stage.addActor(factory.makeMenuButton(game, "Quit", new GameScreen(game, game.FIRST_STAGE_LEVEL_ID)));
+	    }
+	    
+		@Override
+		public void render(float delta) {
 
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
+			Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
+	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-	}
+	        camera.update();
+	        game.batch.setProjectionMatrix(camera.combined);
+	        
+	        game.batch.begin();
+	        game.batch.draw(background, -28, -122, 864, 720);
+	        
+	        // Forces the render
+	        game.batch.flush();
+	        
+	        stage.act();
+	        stage.draw();
+	        game.batch.end();
+			
+			
+		}
 
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
+		@Override
+		public void resize(int width, int height) {
+			// TODO Auto-generated method stub
+			
+		}
 
-	}
+		@Override
+		public void show() {
+			// TODO Auto-generated method stub
+			
+		}
 
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
+		@Override
+		public void hide() {
+			// TODO Auto-generated method stub
+			
+		}
 
-	}
+		@Override
+		public void pause() {
+			// TODO Auto-generated method stub
+			
+		}
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
+		@Override
+		public void resume() {
+			// TODO Auto-generated method stub
+			
+		}
 
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
+		@Override
+		public void dispose() {
+			stage.dispose();
+			
+		}
 
 }
