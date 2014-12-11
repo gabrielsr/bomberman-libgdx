f686d786e0a721775403ae25529311328bb7e298
Gabriel Rodrigues <gabrielsr@gmail.com>
2014-12-10 23:26:08 -0200

    Removing ExplosionSystem do MatchWithUi.

--- core/src/br/unb/unbomber/BomberMatchWithUi.java
+++ core/src/br/unb/unbomber/BomberMatchWithUi.java
@@ -2,7 +2,6 @@ package br.unb.unbomber;
 
 import br.unb.unbomber.core.EntityManager;
 import br.unb.unbomber.systems.AudioSystem;
-import br.unb.unbomber.systems.ExplosionSystem;
 import br.unb.unbomber.systems.LoadStageSystem;
 import br.unb.unbomber.systems.PlayerControlSystem;
 import br.unb.unbomber.systems.RenderSystem;
@@ -45,8 +44,6 @@ public class BomberMatchWithUi extends TargetFrameRateMatch{
 		super.addSystem(new LoadStageSystem(em, stageId));
 		super.addSystem(new PlayerControlSystem(em));
 		super.addSystem(new AudioSystem(em));
-		
-		super.addSystem(new ExplosionSystem(em));
 		super.addSystem(new RenderSystem(em, batch));
 		
 		super.start();
