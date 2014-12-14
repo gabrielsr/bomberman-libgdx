4c409b50875fd2ebee4da8becdb28ed9df832536
Dayof <dayannefernandesc@gmail.com>
2014-12-14 17:15:41 -0200

    Renomeando assets do HUD.

---
+++ core/src/br/unb/unbomber/systems/HUDSystem.java
@@ -0,0 +1,32 @@
+package br.unb.unbomber.systems;
+
+import br.unb.unbomber.core.BaseSystem;
+import br.unb.unbomber.core.EntityManager;
+import br.unb.unbomber.core.Component;
+
+/**
+ * Control the system of the status bar.
+ * 
+ * Create the status bar and update the values of the time e and score.
+ * 
+ * @version 0.1 13 Dez 2014
+ * @author Grupo 5 - Dayanne <dayannefernandesc@gmail.com>
+ * 
+ */
+public class HUDSystem extends BaseSystem {
+	
+	public HUDSystem (EntityManager entityManager) {
+		super(entityManager);
+	}
+	
+	@Override
+	public void start() {
+		//TODO
+	}
+	
+	@Override
+	public void update() {
+		//TODO
+	}
+	
+}
